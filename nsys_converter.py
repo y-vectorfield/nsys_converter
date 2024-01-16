10757904d5638876b79ab17b7f046a68bedc4cb5
Yuhki Yano <isekihakken@keh.biglobe.ne.jp>
2024-01-16 13:50:17 +0900

    Add error handling into covert function

--- nsys_converter.py
+++ nsys_converter.py
@@ -15,7 +15,14 @@ class NsysConverter:
         self.new_sheet_name = new_sheet_name
 
     def convert(self, column_name: str) -> None:
-        file = pd.read_excel(self.file_path, sheet_name=self.sheet_name, header=0)
+        try:
+            file = pd.read_excel(
+                self.file_path, sheet_name=self.sheet_name, header=0)
+        except ValueError as e:
+            print(e)
+            import sys
+            sys.exit()
+
         col_edited = []
         columns = file[column_name]
         for data in columns:
@@ -28,7 +35,8 @@ class NsysConverter:
 
         if self.save_file_name is not None:
             # 指定のファイル名でファイルパスを構築
-            save_path = self.file_path.parent / f'{self.save_file_name}{self.file_path.suffix}'
+            save_path = self.file_path.parent / \
+                f'{self.save_file_name}{self.file_path.suffix}'
         else:
             save_path = f'{self.file_path.stem}_edited{self.file_path.suffix}'
         file.to_excel(save_path, index=False, sheet_name=self.new_sheet_name)
@@ -51,11 +59,17 @@ class NsysConverter:
 if __name__ == '__main__':
     from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--file_path', type=str, required=True, help='File path of Nsyight Systems')
-    parser.add_argument('--sheet_name', type=str, default='Sheet1', help='Sheet name of result file')
-    parser.add_argument('--column_name', type=str, required=True, help='Target column name for convert')
-    parser.add_argument('--save_file_name', type=str, default=None, help='File name for edited sheet')
-    parser.add_argument('--new_sheet_name', type=str, default='Sheet1', help='Sheet name for edited sheet')
+    parser.add_argument('--file_path', type=str, required=True,
+                        help='File path of Nsyight Systems')
+    parser.add_argument('--sheet_name', type=str,
+                        default='Sheet1', help='Sheet name of result file')
+    parser.add_argument('--column_name', type=str,
+                        required=True, help='Target column name for convert')
+    parser.add_argument('--save_file_name', type=str,
+                        default=None, help='File name for edited sheet')
+    parser.add_argument('--new_sheet_name', type=str,
+                        default='Sheet1', help='Sheet name for edited sheet')
     args = parser.parse_args()
-    nc = NsysConverter(args.file_path, args.sheet_name, args.save_file_name, args.new_sheet_name)
+    nc = NsysConverter(args.file_path, args.sheet_name,
+                       args.save_file_name, args.new_sheet_name)
     nc.convert(args.column_name)
